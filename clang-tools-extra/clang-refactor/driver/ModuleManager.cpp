3149ed1c676330f5527a548d7bf9a51c18243f05
Kirill Bobyrev <omtcyfz@gmail.com>
2016-09-08 12:17:07 +0000

    [D24192.70682] [clang-refactor] introducing clang-refactor
    Differential Revision: https://reviews.llvm.org/D24192?id=70682

---
+++ clang-tools-extra/clang-refactor/driver/ModuleManager.cpp
@@ -0,0 +1,30 @@
+//===--- ModuleManager.cpp - clang-refactor ---------------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "ModuleManager.h"
+
+namespace clang {
+namespace refactor {
+
+void ModuleManager::AddModule(StringRef Command,
+                              std::unique_ptr<RefactoringModule> Module) {
+  RegisteredModules.push_back(std::move(Module));
+  CommandToModuleID[Command] = RegisteredModules.size() - 1;
+}
+
+int ModuleManager::Dispatch(StringRef Command, int argc, const char **argv) {
+  auto it = CommandToModuleID.find(Command);
+  if (it != CommandToModuleID.end()) {
+    return RegisteredModules[it->second]->run(argc, argv);
+  }
+  return 1;
+}
+
+} // end namespace refactor
+} // end namespace clang
