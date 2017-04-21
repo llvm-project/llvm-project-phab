c8b8355ab7a0d603b90e775b97132f813a05a9b8
Hal Finkel <hfinkel@anl.gov>
2017-04-21 14:18:52 +0000

    [D32197.96137] [TySan] A Type Sanitizer (Runtime Library)
    Differential Revision: https://reviews.llvm.org/D32197?id=96137

---
+++ compiler-rt/test/tysan/union-wr-wr.c
@@ -0,0 +1,23 @@
+// RUN: %clang_tysan -O0 %s -o %t && %run %t >%t.out 2>&1
+// RUN: FileCheck %s < %t.out
+
+// XFAIL: *
+// Clang's TBAA representation currently get's this wrong (writes to different
+// members of a union type are allowed to alias (i.e. change the type)).
+
+#include <stdio.h>
+
+// CHECK-NOT: ERROR: TypeSanitizer: type-aliasing-violation
+
+int main() {
+  union {
+    int i;
+    short s;
+  } u;
+
+  u.i = 42;
+  u.s = 1;
+
+  printf("%dn", u.i);
+}
+ 
