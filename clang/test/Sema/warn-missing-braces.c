af4215c3bb41f6ecc46ca3ee3693547fa2b14c21
Yuka Takahashi <yukatkh@gmail.com>
2017-04-29 05:37:13 +0000

    [D32646.97182] Fix a bug that -Wmissing-braces fires on system headers
    Differential Revision: https://reviews.llvm.org/D32646?id=97182

--- clang/test/Sema/warn-missing-braces.c
+++ clang/test/Sema/warn-missing-braces.c
@@ -1,3 +1,21 @@
 // RUN: %clang_cc1 -fsyntax-only -Wmissing-braces -verify %s
 
+#ifdef BE_THE_HEADER
+#pragma clang system_header
+
+typedef struct _foo {
+    unsigned char Data[2];
+} foo;
+
+#define BAR { 0 }
+
+#else
+
+#define BE_THE_HEADER
+#include __FILE__
+
 int a[2][2] = { 0, 1, 2, 3 }; // expected-warning{{suggest braces}} expected-warning{{suggest braces}}
+
+foo g = BAR; // should not show warnings
+
+#endif
