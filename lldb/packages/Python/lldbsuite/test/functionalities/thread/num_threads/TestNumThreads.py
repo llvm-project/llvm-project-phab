bd58570de466e50c009362fffa02b092f6de1d0b
Brian Gianforcaro <bgianfo@example.com>
2017-05-24 07:54:20 +0000

    [D33426.100047] Introduce new command: thread backtrace unique
    Differential Revision: https://reviews.llvm.org/D33426?id=100047

--- lldb/packages/Python/lldbsuite/test/functionalities/thread/num_threads/TestNumThreads.py
+++ lldb/packages/Python/lldbsuite/test/functionalities/thread/num_threads/TestNumThreads.py
@@ -20,9 +20,10 @@ class NumberOfThreadsTestCase(TestBase):
         # Call super's setUp().
         TestBase.setUp(self)
         # Find the line number to break inside main().
-        self.line = line_number('main.cpp', '// Set break point at this line.')
+        self.number_of_threads_line = line_number('main.cpp', '// Set number_of_threads break point at this line.')
+        self.unique_stacks_line = line_number('main.cpp', '// Set unique_stacks break point at this line.')
 
-    def test(self):
+    def test_number_of_threads(self):
         """Test number of threads."""
         self.build()
         exe = os.path.join(os.getcwd(), "a.out")
@@ -30,15 +31,15 @@ class NumberOfThreadsTestCase(TestBase):
 
         # This should create a breakpoint with 1 location.
         lldbutil.run_break_set_by_file_and_line(
-            self, "main.cpp", self.line, num_expected_locations=1)
+            self, "main.cpp", self.number_of_threads_line, num_expected_locations=1)
 
-        # The breakpoint list should show 3 locations.
+        # The breakpoint list should show 1 location.
         self.expect(
             "breakpoint list -f",
             "Breakpoint location shown correctly",
             substrs=[
                 "1: file = 'main.cpp', line = %d, exact_match = 0, locations = 1" %
-                self.line])
+                self.number_of_threads_line])
 
         # Run the program.
         self.runCmd("run", RUN_SUCCEEDED)
@@ -59,3 +60,34 @@ class NumberOfThreadsTestCase(TestBase):
         self.assertTrue(
             num_threads >= 4,
             'Number of expected threads and actual threads do not match.')
+        
+    def test_unique_stacks(self):
+        """Test backtrace unique with multiple threads executing the same stack."""
+        self.build()
+        exe = os.path.join(os.getcwd(), "a.out")
+        self.runCmd("file " + exe, CURRENT_EXECUTABLE_SET)
+
+        # This should create a breakpoint with 1 location.
+        lldbutil.run_break_set_by_file_and_line(
+            self, "main.cpp", self.unique_stacks_line, num_expected_locations=1)
+
+        # The breakpoint list should show 1 location.
+        self.expect(
+            "breakpoint list -f",
+            "Breakpoint location shown correctly",
+            substrs=[
+                "1: file = 'main.cpp', line = %d, exact_match = 0, locations = 1" %
+                self.unique_stacks_line])
+
+        # Run the program.
+        self.runCmd("run", RUN_SUCCEEDED)
+
+        # Stopped once.
+        self.expect("thread list", STOPPED_DUE_TO_BREAKPOINT,
+                    substrs=["stop reason = breakpoint 1."])
+
+        # Now that we are stopped, we should have 10 threads waiting in the
+        # thread3 function. All of these threads should show as one stack.
+        self.expect("thread backtrace unique",
+                    "Backtrace with unique stack shown correctly",
+                    substrs=["10 thread(s) #4 #5 #6 #7 #8 #9 #10 #11 #12 #13"])
--- lldb/packages/Python/lldbsuite/test/functionalities/thread/num_threads/main.cpp
+++ lldb/packages/Python/lldbsuite/test/functionalities/thread/num_threads/main.cpp
@@ -1,6 +1,8 @@
+#include <algorithm>
 #include <condition_variable>
 #include <mutex>
 #include <thread>
+#include <vector>
 
 std::mutex mutex;
 std::condition_variable cond;
@@ -9,7 +11,7 @@ void *
 thread3(void *input)
 {
     std::unique_lock<std::mutex> lock(mutex);
-    cond.notify_all(); // Set break point at this line.
+    cond.notify_all(); // Set number_of_threads break point at this line.
     return NULL;
 }
 
@@ -38,13 +40,18 @@ int main()
     std::thread thread_1(thread1, nullptr);
     cond.wait(lock);
 
-    std::thread thread_3(thread3, nullptr);
-    cond.wait(lock);
+    std::vector<std::thread> thread_3s;
+    for (int i = 0; i < 10; i++) {
+      thread_3s.push_back(std::thread(thread3, nullptr));
+    }
+    cond.wait(lock); // Set unique_stacks break point at this line.
 
     lock.unlock();
 
     thread_1.join();
-    thread_3.join();
+    std::for_each(thread_3s.begin(),
+                  thread_3s.end(),
+                  std::mem_fn(&std::thread::join));
 
     return 0;
 }
