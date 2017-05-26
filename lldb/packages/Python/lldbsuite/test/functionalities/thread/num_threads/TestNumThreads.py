b7fbd20d1fdebf72a01ae8b2b659c8c822a4649c
Brian Gianforcaro <bgianfo@example.com>
2017-05-26 05:30:51 +0000

    [D33426.100365] Introduce new command: thread backtrace unique
    Differential Revision: https://reviews.llvm.org/D33426?id=100365

--- lldb/packages/Python/lldbsuite/test/functionalities/thread/num_threads/TestNumThreads.py
+++ lldb/packages/Python/lldbsuite/test/functionalities/thread/num_threads/TestNumThreads.py
@@ -20,9 +20,11 @@ class NumberOfThreadsTestCase(TestBase):
         # Call super's setUp().
         TestBase.setUp(self)
         # Find the line number to break inside main().
-        self.line = line_number('main.cpp', '// Set break point at this line.')
+        self.thread3_notify_all_line = line_number('main.cpp', '// Set thread3 break point on notify_all at this line.')
+        self.thread3_before_lock_line = line_number('main.cpp', '// Set thread3 break point on lock at this line.')
+        self.main_method_first_wait_line = line_number('main.cpp', '// Set break for first wait in thread3 at this line.')
 
-    def test(self):
+    def test_number_of_threads(self):
         """Test number of threads."""
         self.build()
         exe = os.path.join(os.getcwd(), "a.out")
@@ -30,15 +32,15 @@ class NumberOfThreadsTestCase(TestBase):
 
         # This should create a breakpoint with 1 location.
         lldbutil.run_break_set_by_file_and_line(
-            self, "main.cpp", self.line, num_expected_locations=1)
+            self, "main.cpp", self.thread3_notify_all_line, num_expected_locations=1)
 
-        # The breakpoint list should show 3 locations.
+        # The breakpoint list should show 1 location.
         self.expect(
             "breakpoint list -f",
             "Breakpoint location shown correctly",
             substrs=[
                 "1: file = 'main.cpp', line = %d, exact_match = 0, locations = 1" %
-                self.line])
+                self.thread3_notify_all_line])
 
         # Run the program.
         self.runCmd("run", RUN_SUCCEEDED)
@@ -57,5 +59,62 @@ class NumberOfThreadsTestCase(TestBase):
         # Using std::thread may involve extra threads, so we assert that there are
         # at least 4 rather than exactly 4.
         self.assertTrue(
-            num_threads >= 4,
+            num_threads >= 13,
             'Number of expected threads and actual threads do not match.')
+        
+    def test_unique_stacks(self):
+        """Test backtrace unique with multiple threads executing the same stack."""
+        self.build()
+        exe = os.path.join(os.getcwd(), "a.out")
+        self.runCmd("file " + exe, CURRENT_EXECUTABLE_SET)
+
+        # Set a break point in the main method for the first thread3 wait.
+        lldbutil.run_break_set_by_file_and_line(
+            self, "main.cpp", self.main_method_first_wait_line, num_expected_locations=1)
+
+        # The breakpoint list should show 1 location.
+        self.expect(
+            "breakpoint list -f",
+            "Breakpoint location shown correctly",
+            substrs=[
+                "1: file = 'main.cpp', line = %d, exact_match = 0, locations = 1" % self.main_method_first_wait_line])
+
+        # Run the program.
+        self.runCmd("run", RUN_SUCCEEDED)
+
+        # Stopped once.
+        self.expect("thread list", STOPPED_DUE_TO_BREAKPOINT,
+                    substrs=["stop reason = breakpoint 1."])
+
+        # Set a break point on the thread3 notify all (should get hit on threads 4-13).
+        lldbutil.run_break_set_by_file_and_line(
+            self, "main.cpp", self.thread3_before_lock_line, num_expected_locations=1)
+
+        # Continue the program to let all threads go try to hit the break points.
+        self.runCmd("continue", RUN_SUCCEEDED)
+
+        target = self.dbg.GetSelectedTarget()
+        process = target.GetProcess()
+
+        # Get the number of threads
+        num_threads = process.GetNumThreads()
+
+        # Using std::thread may involve extra threads, so we assert that there are
+        # at least 10 thread3's rather than exactly 10.
+        self.assertTrue(
+            num_threads >= 10,
+            'Number of expected threads and actual threads do not match.')
+        
+        # Attempt to walk each of the thread's executing the thread3 function (4 through 13)
+        # to the same breakpoint.
+        for thread_index in range(4, 14):
+            thread = process.GetThreadAtIndex(thread_index)
+            if thread.IsValid():
+                while thread.GetStopReason() != lldb.eStopReasonBreakpoint:
+                    thread.StepInstruction(True)
+
+        # Now that we are stopped, we should have 10 threads waiting in the
+        # thread3 function. All of these threads should show as one stack.
+        self.expect("thread backtrace unique",
+                    "Backtrace with unique stack shown correctly",
+                    substrs=["10 thread(s) #4 #5 #6 #7 #8 #9 #10 #11 #12 #13"])
--- lldb/packages/Python/lldbsuite/test/functionalities/thread/num_threads/main.cpp
+++ lldb/packages/Python/lldbsuite/test/functionalities/thread/num_threads/main.cpp
@@ -1,6 +1,7 @@
 #include <condition_variable>
 #include <mutex>
 #include <thread>
+#include <vector>
 
 std::mutex mutex;
 std::condition_variable cond;
@@ -8,8 +9,14 @@ std::condition_variable cond;
 void *
 thread3(void *input)
 {
-    std::unique_lock<std::mutex> lock(mutex);
-    cond.notify_all(); // Set break point at this line.
+    {
+        // Synchronization of multiple threads before the thread3 breakpoints.
+        std::unique_lock<std::mutex> lock(mutex);
+        cond.notify_all();
+    }
+
+    std::unique_lock<std::mutex> lock(mutex); // Set thread3 break point on lock at this line.
+    cond.notify_all(); // Set thread3 break point on notify_all at this line.
     return NULL;
 }
 
@@ -38,13 +45,21 @@ int main()
     std::thread thread_1(thread1, nullptr);
     cond.wait(lock);
 
-    std::thread thread_3(thread3, nullptr);
+    std::vector<std::thread> thread_3s;
+    for (int i = 0; i < 10; i++) {
+      thread_3s.push_back(std::thread(thread3, nullptr));
+    }
+
+    cond.wait(lock); // Set break for first wait in thread3 at this line.
+
     cond.wait(lock);
 
     lock.unlock();
 
     thread_1.join();
-    thread_3.join();
+    for (auto &t : thread_3s){
+        t.join();
+    }
 
     return 0;
 }
