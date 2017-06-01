cd6e7abe71191190f2aedf46d22631238f8e0642
Edward Jones <edward-jones@example.com>
2017-06-01 08:38:45 +0000

    [D23667.100980] [AAP] (3) Add AAP backend stub
    Differential Revision: https://reviews.llvm.org/D23667?id=100980

---
+++ llvm/lib/Target/AAP/AAPTargetMachine.cpp
@@ -0,0 +1,16 @@
+//===-- AAPTargetMachine.cpp - Define TargetMachine for AAP ---------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// Implements the info about AAP target spec.
+//
+//===----------------------------------------------------------------------===//
+
+extern "C" void LLVMInitializeAAPTarget() {
+}
+
---
+++ llvm/lib/Target/AAP/TargetInfo/AAPTargetInfo.cpp
@@ -0,0 +1,29 @@
+//===-- AAPTargetInfo.cpp - AAP Target Implementation ---------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/IR/Module.h"
+#include "llvm/Support/TargetRegistry.h"
+
+using namespace llvm;
+
+namespace llvm {
+Target &getTheAAPTarget() {
+  static Target TheAAPTarget;
+  return TheAAPTarget;
+}
+}
+
+extern "C" void LLVMInitializeAAPTargetInfo() {
+  RegisterTarget<Triple::aap> X(getTheAAPTarget(), "aap", "AAP [experimental]");
+}
+
+// Placeholder. This will be removed when the MC support has been
+// introduced
+extern "C" void LLVMInitializeAAPTargetMC() {
+}
