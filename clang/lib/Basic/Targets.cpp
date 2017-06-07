89fd25aa1e39f85dbfd17c6553370cf393a4ce46
Kamil Rytarowski <n54@gmx.com>
2017-06-07 23:42:45 +0000

    [D34018.101842] Support __float128 on NetBSD libstdc++ x86/x86_64
    Differential Revision: https://reviews.llvm.org/D34018?id=101842

--- clang/lib/Basic/Targets.cpp
+++ clang/lib/Basic/Targets.cpp
@@ -514,6 +514,8 @@ protected:
     Builder.defineMacro("__ELF__");
     if (Opts.POSIXThreads)
       Builder.defineMacro("_REENTRANT");
+    if (this->HasFloat128)
+      Builder.defineMacro("__FLOAT128__");
 
     switch (Triple.getArch()) {
     default:
@@ -530,6 +532,15 @@ public:
   NetBSDTargetInfo(const llvm::Triple &Triple, const TargetOptions &Opts)
       : OSTargetInfo<Target>(Triple, Opts) {
     this->MCountName = "_mcount";
+
+    switch (Triple.getArch()) {
+    default:
+      break;
+    case llvm::Triple::x86:
+    case llvm::Triple::x86_64:
+      this->HasFloat128 = true;
+      break;
+    }
   }
 };
 
