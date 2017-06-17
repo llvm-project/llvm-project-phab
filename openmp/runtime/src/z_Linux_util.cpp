04d6ada82b2ee45f39eedd479a783fc83c8c4e82
Brian Callahan <bcallah@example.com>
2017-06-17 21:02:21 +0000

    [D34280.102947] Add OpenBSD support to OpenMP
    Differential Revision: https://reviews.llvm.org/D34280?id=102947

--- openmp/runtime/src/z_Linux_util.cpp
+++ openmp/runtime/src/z_Linux_util.cpp
@@ -24,7 +24,7 @@
 #include "kmp_wait_release.h"
 #include "kmp_wrapper_getpid.h"
 
-#if !KMP_OS_FREEBSD && !KMP_OS_NETBSD
+#if !KMP_OS_FREEBSD && !KMP_OS_NETBSD && !KMP_OS_OPENBSD
 #include <alloca.h>
 #endif
 #include <math.h> // HUGE_VAL.
@@ -447,7 +447,7 @@ void __kmp_terminate_thread(int gtid) {
 static kmp_int32 __kmp_set_stack_info(int gtid, kmp_info_t *th) {
   int stack_data;
 #if KMP_OS_LINUX || KMP_OS_FREEBSD || KMP_OS_NETBSD
-  /* Linux* OS only -- no pthread_getattr_np support on OS X* */
+  /* Uses pthread_getattr_np -- not supported on all operating systems */
   pthread_attr_t attr;
   int status;
   size_t size = 0;
@@ -499,7 +499,7 @@ static void *__kmp_launch_worker(void *thr) {
   sigset_t new_set, old_set;
 #endif /* KMP_BLOCK_SIGNALS */
   void *exit_val;
-#if KMP_OS_LINUX || KMP_OS_FREEBSD || KMP_OS_NETBSD
+#if KMP_OS_LINUX || KMP_OS_FREEBSD || KMP_OS_NETBSD || KMP_OS_OPENBSD
   void *volatile padding = 0;
 #endif
   int gtid;
@@ -547,7 +547,7 @@ static void *__kmp_launch_worker(void *thr) {
   KMP_CHECK_SYSFAIL("pthread_sigmask", status);
 #endif /* KMP_BLOCK_SIGNALS */
 
-#if KMP_OS_LINUX || KMP_OS_FREEBSD || KMP_OS_NETBSD
+#if KMP_OS_LINUX || KMP_OS_FREEBSD || KMP_OS_NETBSD || KMP_OS_OPENBSD
   if (__kmp_stkoffset > 0 && gtid > 0) {
     padding = KMP_ALLOCA(gtid * __kmp_stkoffset);
   }
@@ -1787,7 +1787,7 @@ static int __kmp_get_xproc(void) {
 
   int r = 0;
 
-#if KMP_OS_LINUX || KMP_OS_FREEBSD || KMP_OS_NETBSD
+#if KMP_OS_LINUX || KMP_OS_FREEBSD || KMP_OS_NETBSD || KMP_OS_OPENBSD
 
   r = sysconf(_SC_NPROCESSORS_ONLN);
 
@@ -2045,9 +2045,9 @@ int __kmp_is_address_mapped(void *addr) {
     found = 1;
   }; // if
 
-#elif KMP_OS_FREEBSD || KMP_OS_NETBSD
+#elif KMP_OS_FREEBSD || KMP_OS_NETBSD || KMP_OS_OPENBSD
 
-  // FIXME(FreeBSD, NetBSD): Implement this
+  // FIXME(FreeBSD, NetBSD, OpenBSD): Implement this
   found = 1;
 
 #else
