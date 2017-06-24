b0c4f23c5b4bbc9d724577704bed7357be6ea169
Piotr Padlewski <piotr.padlewski@gmail.com>
2017-06-24 10:09:41 +0000

    [D33470.103837] [clang-tidy] Add misc-default-numerics
    Differential Revision: https://reviews.llvm.org/D33470?id=103837

---
+++ clang-tools-extra/test/clang-tidy/misc-default-numerics.cpp
@@ -0,0 +1,42 @@
+// RUN: %check_clang_tidy %s misc-default-numerics %t
+
+namespace std {
+
+template <typename T>
+struct numeric_limits {
+  static T min() { return T(); }
+  static T max() { return T(); }
+};
+
+template <>
+struct numeric_limits<int> {
+  static int min() { return -1; }
+  static int max() { return 1; }
+};
+
+} // namespace std
+
+class MyType {};
+template <typename T>
+class MyTemplate {};
+
+void test() {
+  auto x = std::numeric_limits<int>::min();
+
+  auto y = std::numeric_limits<MyType>::min();
+  // CHECK-MESSAGES: [[@LINE-1]]:12: warning: 'std::numeric_limits::min' called with type 'MyType'; no such specialization exists, so the default value for that type is returned
+
+  auto z = std::numeric_limits<MyTemplate<int>>::max();
+  // CHECK-MESSAGES: [[@LINE-1]]:12: warning: 'std::numeric_limits::max' called with type 'MyTemplate<int>';
+}
+
+template <typename T>
+void fun() {
+  auto x = std::numeric_limits<T>::min();
+}
+
+void testTemplate() {
+  fun<int>();
+  // FIXME: This should generate warning with backtrace.
+  fun<MyType>;
+}
