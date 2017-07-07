ac4ada383f040f578faa2851a7e04e2162cf9a82
Rimvydas <rimvydas@example.com>
2017-07-07 16:35:38 +0000

    [D35129.105656] Add DragonFlyBSD support to OpenMP
    Differential Revision: https://reviews.llvm.org/D35129?id=105656

--- openmp/runtime/src/kmp_platform.h
+++ openmp/runtime/src/kmp_platform.h
@@ -19,6 +19,7 @@
 /* ---------------------- Operating system recognition ------------------- */
 
 #define KMP_OS_LINUX 0
+#define KMP_OS_DRAGONFLY 0
 #define KMP_OS_FREEBSD 0
 #define KMP_OS_NETBSD 0
 #define KMP_OS_DARWIN 0
@@ -46,6 +47,11 @@
 #else
 #endif
 
+#if (defined __DragonFly__)
+#undef KMP_OS_DRAGONFLY
+#define KMP_OS_DRAGONFLY 1
+#endif
+
 #if (defined __FreeBSD__)
 #undef KMP_OS_FREEBSD
 #define KMP_OS_FREEBSD 1
@@ -62,12 +68,13 @@
 #endif
 
 #if (1 !=                                                                      \
-     KMP_OS_LINUX + KMP_OS_FREEBSD + KMP_OS_NETBSD + KMP_OS_DARWIN +           \
-         KMP_OS_WINDOWS)
+     KMP_OS_LINUX + KMP_OS_DRAGONFLY + KMP_OS_FREEBSD + KMP_OS_NETBSD +        \
+         KMP_OS_DARWIN + KMP_OS_WINDOWS)
 #error Unknown OS
 #endif
 
-#if KMP_OS_LINUX || KMP_OS_FREEBSD || KMP_OS_NETBSD || KMP_OS_DARWIN
+#if KMP_OS_LINUX || KMP_OS_DRAGONFLY || KMP_OS_FREEBSD || KMP_OS_NETBSD ||     \
+         KMP_OS_DARWIN
 #undef KMP_OS_UNIX
 #define KMP_OS_UNIX 1
 #endif
