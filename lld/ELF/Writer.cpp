07e8088b503f613918f7f71b7815541b8fe85a7a
Simon Atanasyan <simon@atanasyan.com>
2017-07-07 05:05:26 +0000

    [D31528.105586] [ELF][MIPS] Multi-GOT implementation
    Differential Revision: https://reviews.llvm.org/D31528?id=105586

--- lld/ELF/Writer.cpp
+++ lld/ELF/Writer.cpp
@@ -1245,6 +1245,9 @@ template <class ELFT> void Writer<ELFT>::finalizeSections() {
   if (ErrorCount)
     return;
 
+  if (In<ELFT>::MipsGot)
+    In<ELFT>::MipsGot->template build<ELFT>();
+
   addPredefinedSections();
   removeUnusedSyntheticSections();
 
@@ -1759,8 +1762,6 @@ template <class ELFT> void Writer<ELFT>::fixPredefinedSymbols() {
   if (ElfSym::Bss)
     ElfSym::Bss->Section = findSectionInScript(".bss");
 
-  // Setup MIPS _gp_disp/__gnu_local_gp symbols which should
-  // be equal to the _gp symbol's value.
   if (Config->EMachine == EM_MIPS && !ElfSym::MipsGp->Value) {
     // Find GP-relative section with the lowest address
     // and use this address to calculate default _gp value.
