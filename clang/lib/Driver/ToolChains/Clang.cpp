5c8ee8df1e25181e27d3aae896012bfed7fa7d10
Melanie Blower <melanie.blower@intel.com>
2017-07-10 20:05:48 +0000

    [D34158.105904] For standards compatibility, preinclude <stdc-predef.h> if the file is available
    Differential Revision: https://reviews.llvm.org/D34158?id=105904

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -3175,6 +3175,11 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
       KernelOrKext) {
     CmdArgs.push_back("-ffreestanding");
     IsHosted = false;
+  } else {
+    // For standards compliance, clang will preinclude <stdc-predef.h>
+    // -ffreestanding suppresses this behavior.
+    CmdArgs.push_back("-finclude-if-exists");
+    CmdArgs.push_back("stdc-predef.h");
   }
 
   // Forward -f (flag) options which we can pass directly.
