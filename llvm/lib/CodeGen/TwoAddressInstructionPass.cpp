699d1639e18da9e2080ca8d5026a5916d978a57f
Jesper Antonsson <jesper.antonsson@ericsson.com>
2017-08-03 13:37:22 +0000

    [D36224.109541] [CodeGen} Correctly replace subregister uses when processing tied operands
    Differential Revision: https://reviews.llvm.org/D36224?id=109541

--- llvm/lib/CodeGen/TwoAddressInstructionPass.cpp
+++ llvm/lib/CodeGen/TwoAddressInstructionPass.cpp
@@ -1579,14 +1579,15 @@ TwoAddressInstructionPass::processTiedPairs(MachineInstr *MI,
     if (!IsEarlyClobber) {
       // Replace other (un-tied) uses of regB with LastCopiedReg.
       for (MachineOperand &MO : MI->operands()) {
-        if (MO.isReg() && MO.getReg() == RegB &&
+        if (MO.isReg() && MO.getReg() == RegB && MO.getSubReg() == SubRegB &&
             MO.isUse()) {
           if (MO.isKill()) {
             MO.setIsKill(false);
             RemovedKillFlag = true;
           }
           MO.setReg(LastCopiedReg);
-          MO.setSubReg(MO.getSubReg());
+          if (SubRegB)
+            MO.setSubReg(0);
         }
       }
     }
