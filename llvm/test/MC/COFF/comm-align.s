1ca4dbc926ce21c5da414080132c0d4c108b0669
Martell Malone <martellmalone@gmail.com>
2017-08-06 19:00:11 +0000

    [D36379.109931] LLVM: Make GNU COFF Aligncomm optional
    Differential Revision: https://reviews.llvm.org/D36379?id=109931

--- llvm/test/MC/COFF/comm-align.s
+++ llvm/test/MC/COFF/comm-align.s
@@ -2,8 +2,8 @@
 # RUN:    | llvm-readobj -coff-directives -symbols | FileCheck %s
 
 # NOTE: this test checks multiple things:
-# - that -aligncomm is not emitted for 1-byte alignment
-# - that -aligncomm is emitted for the various alignments (greater than 1)
+# - that -aligncomm is not emitted for 32-byte alignment
+# - that -aligncomm is emitted for the various alignments (greater than 32)
 # - that the alignment is represented as a log_2 of the alignment
 # - that the section switching occurs correctly
 # - that functions after the switch also are emitted into the correct section
@@ -20,11 +20,13 @@ _a:
 	.data
 
 	.comm _s_1,4,0                  # @s_1
-	.comm _s_2,4,1                  # @s_2
-	.comm _s_4,4,2                  # @s_3
-	.comm _s_8,4,3                  # @s_4
+	.comm _s_2,4,2                  # @s_2
+	.comm _s_3,4,4                  # @s_3
+	.comm _s_4,4,8                  # @s_4
 
-	.comm _small_but_overaligned,1,3                  # @s_4
+	.comm _s_5,4,1                  # @s_5
+	.comm _small_but_overalign,1,6  # @s_6
+
 
 	.text
 
@@ -43,8 +45,23 @@ _b:
 # CHECK:     Section: .text (1)
 # CHECK:   }
 # CHECK:   Symbol {
-# CHECK:     Name: _small_but_overaligned
-# CHECK-NEXT:Value: 1
+# CHECK:     Name: _s_1
+# CHECK-NEXT:Value: 4
+# CHECK-NEXT:Section: IMAGE_SYM_UNDEFINED (0)
+# CHECK:   }
+# CHECK:   Symbol {
+# CHECK:     Name: _s_2
+# CHECK-NEXT:Value: 4
+# CHECK-NEXT:Section: IMAGE_SYM_UNDEFINED (0)
+# CHECK:   }
+# CHECK:   Symbol {
+# CHECK:     Name: _s_3
+# CHECK-NEXT:Value: 16
+# CHECK-NEXT:Section: IMAGE_SYM_UNDEFINED (0)
+# CHECK:   }
+# CHECK:   Symbol {
+# CHECK:     Name: _s_5
+# CHECK-NEXT:Value: 4
 # CHECK-NEXT:Section: IMAGE_SYM_UNDEFINED (0)
 # CHECK:   }
 # CHECK:   Symbol {
@@ -53,5 +70,5 @@ _b:
 # CHECK:   }
 # CHECK: ]
 
-# CHECK: Directive(s): -aligncomm:"_s_2",1 -aligncomm:"_s_4",2 -aligncomm:"_s_8",3 -aligncomm:"_small_but_overaligned",3
+# CHECK: Directive(s): -aligncomm:"_s_4",8 -aligncomm:"_small_but_overalign",6
 
