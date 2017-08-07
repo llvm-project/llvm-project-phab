1d0aaafa653224999b0b2eee6e7ad4f778207714
Melanie Blower <melanie.blower@intel.com>
2017-08-07 19:55:07 +0000

    [D34624.110059] extra test modifications for D34158
    Differential Revision: https://reviews.llvm.org/D34624?id=110059

--- clang-tools-extra/test/pp-trace/pp-trace-ident.cpp
+++ clang-tools-extra/test/pp-trace/pp-trace-ident.cpp
@@ -1,4 +1,4 @@
-// RUN: pp-trace -ignore FileChanged,MacroDefined %s -undef -target x86_64 -std=c++11 | FileCheck --strict-whitespace %s
+// RUN: pp-trace -ignore FileChanged,MacroDefined %s -undef -ffreestanding -target x86_64 -std=c++11 | FileCheck --strict-whitespace %s
 
 #ident "$Id$"
 
