eac0d12d27ead2128885b806270cddacd5842a47
George Rimar <grimar@accesssoftek.com>
2017-08-23 09:01:57 +0000

    [D35945.112313] [ELF] - Linkerscript: better diagnostic for INPUT/GROUP commands.
    Differential Revision: https://reviews.llvm.org/D35945?id=112313

--- lld/ELF/ScriptParser.cpp
+++ lld/ELF/ScriptParser.cpp
@@ -251,16 +251,16 @@ void ScriptParser::readLinkerScript() {
 }
 
 void ScriptParser::addFile(StringRef S) {
-  if (IsUnderSysroot && S.startswith("/")) {
-    SmallString<128> PathData;
-    StringRef Path = (Config->Sysroot + S).toStringRef(PathData);
-    if (sys::fs::exists(Path)) {
-      Driver->addFile(Saver.save(Path), /*WithLOption=*/false);
-      return;
-    }
-  }
+  assert(!ErrorCount);
 
   if (S.startswith("/")) {
+    if (IsUnderSysroot) {
+      SmallString<128> PathData;
+      StringRef Path = (Config->Sysroot + S).toStringRef(PathData);
+      if (sys::fs::exists(Path))
+        S = Saver.save(Path);
+    }
+
     Driver->addFile(S, /*WithLOption=*/false);
   } else if (S.startswith("=")) {
     if (Config->Sysroot.empty())
@@ -278,6 +278,9 @@ void ScriptParser::addFile(StringRef S) {
     else
       setError("unable to find " + S);
   }
+
+  if (ErrorCount)
+    error(">>> location:" + getCurrentLocation());
 }
 
 void ScriptParser::readAsNeeded() {
