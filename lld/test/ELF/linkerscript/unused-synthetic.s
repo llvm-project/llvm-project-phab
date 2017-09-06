9d9f5fcb5482a3d2bbe4730046fc335df3c8a62c
George Rimar <grimar@accesssoftek.com>
2017-09-06 15:23:40 +0000

    [D37520.114013] [ELF] - Fix removing of unused synthetic sections.
    Differential Revision: https://reviews.llvm.org/D37520?id=114013

--- lld/test/ELF/linkerscript/unused-synthetic.s
+++ lld/test/ELF/linkerscript/unused-synthetic.s
@@ -1,9 +1,9 @@
 # REQUIRES: x86
 # RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-linux %s -o %t.o
-# RUN: echo "SECTIONS { \
-# RUN:    .got  : { *(.got) } \
-# RUN:    .plt  : { *(.plt) } \
-# RUN:    .text : { *(.text) } \
+# RUN: echo "SECTIONS {                             \
+# RUN:    .got  : { *(.got) *(.got) }               \
+# RUN:    .plt  : { *(.plt) }                       \
+# RUN:    .text : { *(.text) }                      \
 # RUN:  }" > %t.script
 # RUN: ld.lld -shared -o %t.so --script %t.script %t.o
 
