366a27de8de7cb5956931e114b23b9677a569fa2
Stephen Crane <sjc@immunant.com>
2017-09-07 19:21:06 +0000

    [D37580.114232] Add Position Independent Pages (PIP) relocation model
    Differential Revision: https://reviews.llvm.org/D37580?id=114232

--- llvm/tools/gold/gold-plugin.cpp
+++ llvm/tools/gold/gold-plugin.cpp
@@ -237,6 +237,8 @@ namespace options {
       DisableVerify = true;
     } else if (opt.startswith("sample-profile=")) {
       sample_profile= opt.substr(strlen("sample-profile="));
+    } else if (opt == "pagerando") {
+      RelocationModel = Reloc::PIP;
     } else {
       // Save this option to pass to the code generator.
       // ParseCommandLineOptions() expects argv[0] to be program name. Lazily
@@ -286,15 +288,15 @@ ld_plugin_status onload(ld_plugin_tv *tv) {
         break;
       case LDPO_DYN: // .so
         IsExecutable = false;
-        RelocationModel = Reloc::PIC_;
+        RelocationModel = RelocationModel ? RelocationModel : Reloc::PIC_;
         break;
       case LDPO_PIE: // position independent executable
         IsExecutable = true;
-        RelocationModel = Reloc::PIC_;
+        RelocationModel = RelocationModel ? RelocationModel : Reloc::PIC_;
         break;
       case LDPO_EXEC: // .exe
         IsExecutable = true;
-        RelocationModel = Reloc::Static;
+        RelocationModel = RelocationModel ? RelocationModel : Reloc::Static;
         break;
       default:
         message(LDPL_ERROR, "Unknown output file type %d", tv->tv_u.tv_val);
