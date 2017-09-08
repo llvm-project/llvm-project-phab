2db08e510320f2319fda94c7e06bac4a2dc6cbff
Michael J. Spencer <bigcheesegs@gmail.com>
2017-09-08 22:55:46 +0000

    [D36351.114448] [lld][ELF] Add profile guided section layout
    Differential Revision: https://reviews.llvm.org/D36351?id=114448

--- lld/ELF/Writer.cpp
+++ lld/ELF/Writer.cpp
@@ -8,6 +8,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "Writer.h"
+#include "CallGraphSort.h"
 #include "Config.h"
 #include "Filesystem.h"
 #include "LinkerScript.h"
@@ -916,6 +917,20 @@ template <class ELFT> void Writer<ELFT>::createSections() {
                               Old.end());
 
   Script->fabricateDefaultCommands();
+
+  // Use the rarely used option -call-graph-ordering-file to sort sections.
+  if (!Config->CallGraphProfile.empty()) {
+    DenseMap<const InputSectionBase *, int> OrderMap =
+        computeCallGraphProfileOrder();
+
+    for (BaseCommand *Base : Script->Opt.Commands)
+      if (auto *OS = dyn_cast<OutputSection>(Base))
+        if (OS->Name == ".text") {
+          OS->sort([&](InputSectionBase *IS) { return OrderMap.lookup(IS); });
+          break;
+        }
+  }
+
   sortBySymbolsOrder<ELFT>();
   sortInitFini(findSection(".init_array"));
   sortInitFini(findSection(".fini_array"));
