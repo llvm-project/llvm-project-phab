8fb84f0fc2673cfaed24b72bf308b3297ace5a03
Leslie Zhai <lesliezhai@llvm.org.cn>
2017-09-11 08:59:46 +0000

    [D37615.114546] [ELF] Implement the relocations of AVR for LLD
    Differential Revision: https://reviews.llvm.org/D37615?id=114546

--- lld/test/ELF/basic-avr.s
+++ lld/test/ELF/basic-avr.s
@@ -4,11 +4,31 @@
 # RUN: llvm-objdump -d %t.exe | FileCheck %s
 
 main:
-  call foo
+  ldi r17, lo8(foo)
+  ldi r18, hi8(bar)
+  ldi r19, hh8(foo)
+  ldi r20, foo
+  brne next
+  rjmp next
+  adiw r24, bar
+next:
+  jmp next
 foo:
   jmp foo
+bar:
+  jmp bar
 
 # CHECK:      main:
-# CHECK-NEXT:   0: 0e 94 02 00 <unknown>
+# CHECK-NEXT:   0: 12 e1 <unknown>
+# CHECK-NEXT:   2: 20 e0 <unknown>
+# CHECK-NEXT:   4: 30 e0 <unknown>
+# CHECK-NEXT:   6: 42 e1 <unknown>
+# CHECK-NEXT:   8: 11 f4 <unknown>
+# CHECK-NEXT:   a: 01 c0 <unknown>
+# CHECK-NEXT:   c: 46 96 <unknown>
+# CHECK:      next:
+# CHECK-NEXT:   e: 0c 94 07 00 <unknown>
 # CHECK:      foo:
-# CHECK-NEXT:   4: 0c 94 02 00 <unknown>
+# CHECK-NEXT:  12: 0c 94 09 00 <unknown>
+# CHECK:      bar:
+# CHECK-NEXT:  16: 0c 94 0b 00 <unknown>
