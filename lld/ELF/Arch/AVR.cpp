f3c596083cb9975052fc3267f1da79ca9a334929
Leslie Zhai <lesliezhai@llvm.org.cn>
2017-09-13 05:35:03 +0000

    [D37615.114972] [ELF] Implement the relocations of AVR for LLD
    Differential Revision: https://reviews.llvm.org/D37615?id=114972

--- lld/ELF/Arch/AVR.cpp
+++ lld/ELF/Arch/AVR.cpp
@@ -35,12 +35,30 @@
 
 using namespace llvm;
 using namespace llvm::object;
+using namespace llvm::support;
 using namespace llvm::support::endian;
 using namespace llvm::ELF;
 using namespace lld;
 using namespace lld::elf;
 
 namespace {
+uint16_t calculateForLDI(uint16_t X, uint16_t Val) {
+  return (X & 0xf0f0) | (Val & 0xf) | ((Val << 4) & 0xf00);
+}
+
+// All PC-relative instructions need to be adjusted by -2 IIRC to account for
+// the size of the current instruction.
+void adjustRelativeBranch(uint16_t &Val) {
+  Val -= 2; // Branch instructions add 2 to the PC.
+}
+
+// Adjusts a program memory address. This is a simple right-shift.
+template <typename T = uint16_t> void adjustPM(T &Val) {
+  Val >>= 1; // AVR addresses commands as words.
+}
+
+void adjustNeg(int16_t &Val) { Val *= -1; }
+
 class AVR final : public TargetInfo {
 public:
   RelExpr getRelExpr(uint32_t Type, const SymbolBody &S, const InputFile &File,
@@ -52,7 +70,38 @@ public:
 RelExpr AVR::getRelExpr(uint32_t Type, const SymbolBody &S,
                         const InputFile &File, const uint8_t *Loc) const {
   switch (Type) {
+  case R_AVR_7_PCREL:
+  case R_AVR_13_PCREL:
+    return R_PC;
+  case R_AVR_LO8_LDI:
+  case R_AVR_LDI:
+  case R_AVR_6:
+  case R_AVR_6_ADIW:
+  case R_AVR_HI8_LDI:
+  case R_AVR_HH8_LDI:
+  case R_AVR_MS8_LDI:
+  case R_AVR_LO8_LDI_NEG:
+  case R_AVR_HI8_LDI_NEG:
+  case R_AVR_HH8_LDI_NEG:
+  case R_AVR_MS8_LDI_NEG:
+  case R_AVR_LO8_LDI_GS:
+  case R_AVR_LO8_LDI_PM:
+  case R_AVR_HI8_LDI_GS:
+  case R_AVR_HI8_LDI_PM:
+  case R_AVR_HH8_LDI_PM:
+  case R_AVR_LO8_LDI_PM_NEG:
+  case R_AVR_HI8_LDI_PM_NEG:
+  case R_AVR_HH8_LDI_PM_NEG:
+  case R_AVR_8:
+  case R_AVR_8_LO8:
+  case R_AVR_8_HI8:
+  case R_AVR_8_HLO8:
   case R_AVR_CALL:
+  case R_AVR_16:
+  case R_AVR_16_PM:
+  case R_AVR_LDS_STS_16:
+  case R_AVR_PORT6:
+  case R_AVR_PORT5:
     return R_ABS;
   default:
     error(toString(&File) + ": unknown relocation type: " + toString(Type));
@@ -61,14 +110,149 @@ RelExpr AVR::getRelExpr(uint32_t Type, const SymbolBody &S,
 }
 
 void AVR::relocateOne(uint8_t *Loc, uint32_t Type, uint64_t Val) const {
+  uint16_t SRel = Val;
+  uint16_t X = read16le(Loc);
   switch (Type) {
-  case R_AVR_CALL: {
-    uint16_t Hi = Val >> 17;
-    uint16_t Lo = Val >> 1;
-    write16le(Loc, read16le(Loc) | ((Hi >> 1) << 4) | (Hi & 1));
-    write16le(Loc + 2, Lo);
+  case R_AVR_7_PCREL:
+    adjustRelativeBranch(SRel);
+    X = (X & 0xfc07) | (((SRel >> 1) << 3) & 0x3f8);
+    write16le(Loc, X);
+    break;
+  case R_AVR_13_PCREL:
+    adjustRelativeBranch(SRel);
+    adjustPM(SRel);
+    X = (X & 0xf000) | (SRel & 0xfff);
+    write16le(Loc, X);
+    break;
+  case R_AVR_LO8_LDI:
+  case R_AVR_LDI:
+    write16le(Loc, calculateForLDI(X, SRel));
+    break;
+  case R_AVR_6:
+    X = (X & 0xd3f8) | ((SRel & 7) | ((SRel & (3 << 3)) << 7)
+                     | ((SRel & (1 << 5)) << 8));
+    write16le(Loc, X);
+    break;
+  case R_AVR_6_ADIW:
+    X = (X & 0xff30) | (SRel & 0xf) | ((SRel & 0x30) << 2);
+    write16le(Loc, X);
+    break;
+  case R_AVR_HI8_LDI:
+    SRel = (SRel >> 8) & 0xff;
+    write16le(Loc, calculateForLDI(X, SRel));
+    break;
+  case R_AVR_HH8_LDI:
+    SRel = (SRel >> 16) & 0xff;
+    write16le(Loc, calculateForLDI(X, SRel));
+    break;
+  case R_AVR_MS8_LDI:
+    SRel = (SRel >> 24) & 0xff;
+    write16le(Loc, calculateForLDI(X, SRel));
+    break;
+  case R_AVR_LO8_LDI_NEG: {
+    int16_t SRel = Val;
+    adjustNeg(SRel);
+    write16le(Loc, calculateForLDI(X, (uint16_t)SRel));
     break;
   }
+  case R_AVR_HI8_LDI_NEG: {
+    int16_t SRel = Val;
+    adjustNeg(SRel);
+    SRel = (SRel >> 8) & 0xff;
+    write16le(Loc, calculateForLDI(X, (uint16_t)SRel));
+    break;
+  }
+  case R_AVR_HH8_LDI_NEG: {
+    int16_t SRel = Val;
+    adjustNeg(SRel);
+    SRel = (SRel >> 16) & 0xff;
+    write16le(Loc, calculateForLDI(X, (uint16_t)SRel));
+    break;
+  }
+  case R_AVR_MS8_LDI_NEG: {
+    int16_t SRel = Val;
+    adjustNeg(SRel);
+    SRel = (SRel >> 24) & 0xff;
+    write16le(Loc, calculateForLDI(X, (uint16_t)SRel));
+    break;
+  }
+  case R_AVR_LO8_LDI_GS:
+  case R_AVR_LO8_LDI_PM:
+    adjustPM(SRel);
+    write16le(Loc, calculateForLDI(X, SRel));
+    break;
+  case R_AVR_HI8_LDI_GS:
+  case R_AVR_HI8_LDI_PM:
+    adjustPM(SRel);
+    SRel = (SRel >> 8) & 0xff;
+    write16le(Loc, calculateForLDI(X, SRel));
+    break;
+  case R_AVR_HH8_LDI_PM:
+    adjustPM(SRel);
+    SRel = (SRel >> 16) & 0xff;
+    write16le(Loc, calculateForLDI(X, SRel));
+    break;
+  case R_AVR_LO8_LDI_PM_NEG: {
+    int16_t SRel = Val;
+    adjustNeg(SRel);
+    adjustPM<int16_t>(SRel);
+    write16le(Loc, calculateForLDI(X, (uint16_t)SRel));
+    break;
+  }
+  case R_AVR_HI8_LDI_PM_NEG: {
+    int16_t SRel = Val;
+    adjustNeg(SRel);
+    adjustPM<int16_t>(SRel);
+    SRel = (SRel >> 8) & 0xff;
+    write16le(Loc, calculateForLDI(X, (uint16_t)SRel));
+    break;
+  }
+  case R_AVR_HH8_LDI_PM_NEG: {
+    int16_t SRel = Val;
+    adjustNeg(SRel);
+    adjustPM<int16_t>(SRel);
+    SRel = (SRel >> 16) & 0xff;
+    write16le(Loc, calculateForLDI(X, (uint16_t)SRel));
+    break;
+  }
+  case R_AVR_8:
+    write16le(Loc, SRel & 0x000000ff);
+    break;
+  case R_AVR_8_LO8:
+    write16le(Loc, SRel & 0xffffff);
+    break;
+  case R_AVR_8_HI8:
+    write16le(Loc, (SRel >> 8) & 0xffffff);
+    break;
+  case R_AVR_8_HLO8:
+    write16le(Loc, (SRel >> 16) & 0xffffff);
+    break;
+  case R_AVR_CALL:
+    adjustPM(SRel);
+    X |= ((SRel & 0x10000) | ((SRel << 3) & 0x1f00000)) >> 16;
+    write16le(Loc, X);
+    write16le(Loc + 2, SRel & 0xffff);
+    break;
+  case R_AVR_16:
+    write16le(Loc, SRel & 0x00ffff);
+    break;
+  case R_AVR_16_PM:
+    adjustPM(SRel);
+    write16le(Loc, SRel & 0x00ffff);
+    break;
+  case R_AVR_LDS_STS_16:
+    SRel = SRel & 0x7f;
+    X |= (SRel & 0x0f) | ((SRel & 0x30) << 5) | ((SRel & 0x40) << 2);
+    write16le(Loc, X);
+    break;
+  case R_AVR_PORT6:
+    X = (X & 0xf9f0) | ((SRel & 0x30) << 5) | (SRel & 0x0f);
+    write16le(Loc, X);
+    break;
+  case R_AVR_PORT5:
+    X = (X & 0xff07) | ((SRel & 0x1f) << 3);
+    write16le(Loc, X);
+    break;
   default:
     error(getErrorLocation(Loc) + "unrecognized reloc " + toString(Type));
   }
