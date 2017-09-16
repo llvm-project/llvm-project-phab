8093681fa5e07ffee731b958e7b05f7c7b69181a
Serge Pavlov <sepavloff@gmail.com>
2017-09-16 15:40:06 +0000

    [D21508.115539] Diagnose friend function template redefinitions
    Differential Revision: https://reviews.llvm.org/D21508?id=115539

--- clang/lib/AST/Decl.cpp
+++ clang/lib/AST/Decl.cpp
@@ -3267,6 +3267,14 @@ FunctionDecl *FunctionDecl::getTemplateInstantiationPattern() const {
   if (auto *MFD = getInstantiatedFromMemberFunction())
     return getDefinitionOrSelf(MFD);
 
+  if (FunctionTemplateDecl *TD = getDescribedFunctionTemplate()) {
+    if (TD->getFriendObjectKind() != FOK_None) {
+      while (FunctionTemplateDecl *FT = TD->getInstantiatedFromMemberTemplate())
+        TD = FT;
+      return TD->getTemplatedDecl();
+    }
+  }
+
   return nullptr;
 }
 
