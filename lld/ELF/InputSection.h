2942855d42367fbe3002d12cc14da8d44dd045a7
George Rimar <grimar@accesssoftek.com>
2017-09-18 15:03:28 +0000

    [D35987.115654] [ELF] - Introduce std::vector<InputFile *> global arrays.
    Differential Revision: https://reviews.llvm.org/D35987?id=115654

--- lld/ELF/InputSection.h
+++ lld/ELF/InputSection.h
@@ -337,7 +337,7 @@ private:
 extern std::vector<InputSectionBase *> InputSections;
 
 // Builds section order for handling --symbol-ordering-file.
-template <class ELFT> llvm::DenseMap<SectionBase *, int> buildSectionOrder();
+llvm::DenseMap<SectionBase *, int> buildSectionOrder();
 
 } // namespace elf
 
