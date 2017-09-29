39657bc16819e3bf2348e9d5638c4c35a7c6b862
ben <bd1976llvm@example.com>
2017-09-29 12:47:19 +0000

    [D38401.117129] [ELF] Fix incorrect exit status bug exposed by D38283
    Differential Revision: https://reviews.llvm.org/D38401?id=117129

--- lld/ELF/Writer.cpp
+++ lld/ELF/Writer.cpp
@@ -246,6 +246,8 @@ template <class ELFT> void Writer<ELFT>::run() {
 
   if (auto EC = Buffer->commit())
     error("failed to write to the output file: " + EC.message());
+  if (ErrorCount)
+    return;
 
   // Flush the output streams and exit immediately. A full shutdown
   // is a good test that we are keeping track of all allocated memory,
