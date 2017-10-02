f04f50cb4da745fe5c2f64402a7ace7a22610a0b
George Rimar <grimar@accesssoftek.com>
2017-10-02 10:51:23 +0000

    [D37520.117320] [ELF] - Fix removing of unused synthetic sections.
    Differential Revision: https://reviews.llvm.org/D37520?id=117320

--- lld/ELF/Writer.cpp
+++ lld/ELF/Writer.cpp
@@ -1180,22 +1180,23 @@ static void removeUnusedSyntheticSections() {
     if (!SS->empty() || !OS)
       continue;
 
-    std::vector<BaseCommand *>::iterator Empty = OS->Commands.end();
-    for (auto I = OS->Commands.begin(), E = OS->Commands.end(); I != E; ++I) {
-      BaseCommand *B = *I;
+    // SS is an unused synthetic section. Remove it from output section command.
+    for (BaseCommand *B : OS->Commands) {
       if (auto *ISD = dyn_cast<InputSectionDescription>(B)) {
-        llvm::erase_if(ISD->Sections,
-                       [=](InputSection *IS) { return IS == SS; });
-        if (ISD->Sections.empty())
-          Empty = I;
+        auto It = llvm::find(ISD->Sections, SS);
+        if (It == ISD->Sections.end())
+          continue;
+        ISD->Sections.erase(It);
+        break;
       }
     }
-    if (Empty != OS->Commands.end())
-      OS->Commands.erase(Empty);
 
-    // If there are no other sections in the output section, remove it from the
-    // output.
-    if (OS->Commands.empty())
+    // If SS was the only section content, remove the entire output section.
+    bool IsEmpty = llvm::all_of(OS->Commands, [](BaseCommand *B) {
+      auto *ISD = dyn_cast<InputSectionDescription>(B);
+      return ISD && ISD->Sections.empty();
+    });
+    if (IsEmpty)
       OS->Live = false;
   }
 }
