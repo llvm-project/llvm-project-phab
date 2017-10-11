23d17f884a5a8a24f7d88d7557fc038a655137a3
Simon Atanasyan <simon@atanasyan.com>
2017-10-11 10:39:32 +0000

    [D31528.118583] [ELF][MIPS] Multi-GOT implementation
    Differential Revision: https://reviews.llvm.org/D31528?id=118583

--- lld/ELF/Writer.cpp
+++ lld/ELF/Writer.cpp
@@ -1298,6 +1298,9 @@ template <class ELFT> void Writer<ELFT>::finalizeSections() {
   if (ErrorCount)
     return;
 
+  if (In<ELFT>::MipsGot)
+    In<ELFT>::MipsGot->template build<ELFT>();
+
   addPredefinedSections();
   removeUnusedSyntheticSections();
 
