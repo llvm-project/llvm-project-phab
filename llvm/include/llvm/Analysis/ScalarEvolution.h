22a30e2b3a5430deccbc1f085a737437aa01ddbd
Jatin Bhateja <jatin.bhateja@gmail.com>
2017-10-17 02:28:11 +0000

    [D38494.119241] [ScalarEvolution] Handling for ICmp occuring in the evolution chain.
    Differential Revision: https://reviews.llvm.org/D38494?id=119241

--- llvm/include/llvm/Analysis/ScalarEvolution.h
+++ llvm/include/llvm/Analysis/ScalarEvolution.h
@@ -514,7 +514,7 @@ public:
 
   /// Erase Value from ValueExprMap and ExprValueMap.
   void eraseValueFromMap(Value *V);
-
+  
   /// Return a SCEV expression for the full generality of the specified
   /// expression.
   const SCEV *getSCEV(Value *V);
@@ -1382,6 +1382,9 @@ private:
 
   /// Helper function called from createNodeForPHI.
   const SCEV *createAddRecFromPHI(PHINode *PN);
+
+  /// Evaluate ICmpInst to a constant node for special patterns.
+  const SCEV *evaluateForICmp(ICmpInst *IC);
 
   /// A helper function for createAddRecFromPHI to handle simple cases.
   const SCEV *createSimpleAffineAddRec(PHINode *PN, Value *BEValueV,
