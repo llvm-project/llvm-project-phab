fb4b542de96d55e4e43835779024f56d49831027
Peter Smith <peter.smith@linaro.org>
2017-11-06 11:46:57 +0000

    [D36749.121714] [LLD][ELF][AArch64] Complete implementation of -fix-cortex-a53-843419
    Differential Revision: https://reviews.llvm.org/D36749?id=121714

--- lld/ELF/SectionPatcher.h
+++ lld/ELF/SectionPatcher.h
@@ -12,17 +12,45 @@
 
 #include "lld/Common/LLVM.h"
 
+#include <map>
+#include <vector>
+
 namespace lld {
 namespace elf {
 
+class Defined;
+class InputSection;
+class InputSectionDescription;
 class OutputSection;
+class Patch843419Section;
 
 // Implementation of the -fix-cortex-a53-843419 which affects early revisions
-// of the cortex-a53 when running in the AArch64 execution state.
+// of the cortex-a53 when running in the AArch64 execution state. The erratum
+// occurs when a certain sequence of instructions occur on a 4k page boundary.
+// To workaround this problem the linker will scan for these sequences and will
+// replace one of the instructions with a branch to a thunk called a patch that
+// will execute the instruction and return to the instruction after the branch
+// to the patch. The replacement of the instruction with a branch is
+// sufficient to prevent the erratum.
+class SectionPatcher {
+public:
+  SectionPatcher();
 
-// FIXME: Only detects and reports the presence of the instruction sequence that
-// can trigger the erratum 843419.
-void createA53Errata843419Fixes(ArrayRef<OutputSection *> OutputSections);
+  // return true if Patches have been added to the OutputSections.
+  bool create843419Fixes(ArrayRef<OutputSection *> OutputSections);
+
+private:
+  void patchInputSectionDescription(InputSectionDescription &ISD,
+                                    std::vector<Patch843419Section *> &Patches);
+
+  void insertPatches(InputSectionDescription &ISD,
+                     std::vector<Patch843419Section *> &Patches);
+
+  // A cache of the mapping symbols defined by the InputSecion sorted in order
+  // of ascending value with redundant symbols removed. These describe
+  // the ranges of code and data in an executable InputSection.
+  std::map<InputSection *, std::vector<const Defined *>> SectionMap;
+};
 
 } // namespace elf
 } // namespace lld
