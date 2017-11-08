0236a119332d68cb88b785a2ee210e6e7edb836b
Nemanja Ivanovic <nemanja.i.ibm@gmail.com>
2017-11-08 09:47:38 +0000

    [D39785.122041] [PowerPC] Remove redundant register copies in late peephole
    Differential Revision: https://reviews.llvm.org/D39785?id=122041

--- llvm/lib/Target/PowerPC/PPCPreEmitPeephole.cpp
+++ llvm/lib/Target/PowerPC/PPCPreEmitPeephole.cpp
@@ -33,6 +33,8 @@ STATISTIC(NumRRConvertedInPreEmit,
           "Number of r+r instructions converted to r+i in pre-emit peephole");
 STATISTIC(NumRemovedInPreEmit,
           "Number of instructions deleted in pre-emit peephole");
+STATISTIC(NumRedundantPairsFound,
+          "Number of redundant pair instructions found in pre-emit peephole");
 
 static cl::opt<bool>
 RunPreEmitPeephole("ppc-late-peephole", cl::Hidden, cl::init(false),
@@ -40,6 +42,7 @@ RunPreEmitPeephole("ppc-late-peephole", cl::Hidden, cl::init(false),
 
 namespace {
   class PPCPreEmitPeephole : public MachineFunctionPass {
+    MachineInstr *hasRedundantPair(MachineInstr &MI) const;
   public:
     static char ID;
     PPCPreEmitPeephole() : MachineFunctionPass(ID) {
@@ -64,6 +67,7 @@ namespace {
       for (MachineBasicBlock &MBB : MF) {
         for (MachineInstr &MI : MBB) {
           MachineInstr *DefMIToErase = nullptr;
+          MachineInstr *RedundantPairMI = nullptr;
           if (TII->convertToImmediateForm(MI, &DefMIToErase)) {
             Changed = true;
             NumRRConvertedInPreEmit++;
@@ -72,18 +76,65 @@ namespace {
             if (DefMIToErase) {
               InstrsToErase.push_back(DefMIToErase);
             }
+          } else if ((RedundantPairMI = hasRedundantPair(MI))) {
+            InstrsToErase.push_back(RedundantPairMI);
+            NumRedundantPairsFound++;
           }
         }
       }
       for (MachineInstr *MI : InstrsToErase) {
         DEBUG(dbgs() << "PPC pre-emit peephole: erasing instruction: ");
         DEBUG(MI->dump());
+        assert(MI->getParent() &&
+               "The same instruction marked redundant multiple times?");
         MI->eraseFromParent();
         NumRemovedInPreEmit++;
       }
       return Changed;
     }
   };
+
+  // For now, only handle mr X, Y -> mr Y, X with no clobber of Y in between
+  // as that is the only pattern known to come up.
+  MachineInstr *PPCPreEmitPeephole::hasRedundantPair(MachineInstr &MI) const {
+    unsigned Opc = MI.getOpcode();
+
+    // If this isn't an mr, return null.
+    if ((Opc != PPC::OR && Opc != PPC::OR8) ||
+        MI.getOperand(1).getReg() != MI.getOperand(2).getReg())
+      return nullptr;
+
+    unsigned DestReg = MI.getOperand(0).getReg();
+    unsigned SrcReg = MI.getOperand(1).getReg();
+    const MachineRegisterInfo *MRI = &MI.getParent()->getParent()->getRegInfo();
+    const TargetRegisterInfo *TRI = MRI->getTargetRegisterInfo();
+
+    MachineBasicBlock::iterator StartIt = MI, EndIt = MI.getParent()->end();
+    StartIt++;
+    for (; StartIt != EndIt; ++StartIt) {
+      // If we encounter an instruction that clobbers the destination,
+      // any subsequent copy of the original destination won't be copying
+      // the same register, return null.
+      if (StartIt->modifiesRegister(DestReg, TRI))
+        return nullptr;
+
+      // If we find an instruction that clobbers the source register, it is
+      // redundant if it's a copy from the original destination and we haven't
+      // exited above.
+      if (StartIt->modifiesRegister(SrcReg, TRI)) {
+        if (StartIt->getOpcode() != Opc ||
+            StartIt->getOperand(1).getReg() != DestReg ||
+            StartIt->getOperand(2).getReg() != DestReg)
+          return nullptr;
+
+        // Clear the kill flags from the original instruction.
+        MI.getOperand(1).setIsKill(false);
+        MI.getOperand(2).setIsKill(false);
+        return &*StartIt;
+      }
+    }
+    return nullptr;
+  }
 }
 
 INITIALIZE_PASS(PPCPreEmitPeephole, DEBUG_TYPE, "PowerPC Pre-Emit Peephole",
