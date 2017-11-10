f41056d3e1a53468b5ae1be9a733054e63268d81
Jake Ehrlich <jakehehrlich@google.com>
2017-11-10 20:57:15 +0000

    [D39919.122506] [llvm-objcopy] Add --strip-debug
    Differential Revision: https://reviews.llvm.org/D39919?id=122506

--- llvm/tools/llvm-objcopy/llvm-objcopy.cpp
+++ llvm/tools/llvm-objcopy/llvm-objcopy.cpp
@@ -81,6 +81,8 @@ static cl::list<std::string> ToRemove("remove-section",
                                       cl::desc("Remove a specific section"));
 static cl::alias ToRemoveA("R", cl::desc("Alias for remove-section"),
                            cl::aliasopt(ToRemove));
+static cl::opt<bool> StripDebug("strip-debug",
+                                cl::desc("Removes all debug information"));
 static cl::opt<bool> StripSections("strip-sections",
                                    cl::desc("Remove all section headers"));
 static cl::opt<bool>
@@ -175,6 +177,12 @@ void CopyBinary(const ELFObjectFile<ELF64LE> &ObjFile) {
       return RemovePred(Sec) || (Sec.Flags & SHF_ALLOC) == 0;
     };
     Obj->WriteSectionHeaders = false;
+  }
+
+  if (StripDebug) {
+    RemovePred = [RemovePred](const SectionBase &Sec) {
+      return RemovePred(Sec) || Sec.Name.startswith(".debug");
+    };
   }
 
   Obj->removeSections(RemovePred);
